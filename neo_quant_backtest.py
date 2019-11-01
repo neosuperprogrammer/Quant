b2a3daf3efbb1a5198b02f2e3253e941b1668267
namsangwook <flowgrammer@gmail.com>
2019-11-02 00:15:53 +0900

    test

---
+++ neo_quant_backtest.py
@@ -0,0 +1,214 @@
+# %load neo_quant.py
+import pandas as pd
+import numpy as np
+# import requests
+# import bs4
+# import time
+# import datetime
+# import matplotlib.pyplot as plt
+# from dateutil import parser
+# from matplotlib import font_manager, rc
+# from IPython.display import HTML
+# pd.set_option('display.max_colwidth', -1)
+
+rc('font', family='AppleGothic')
+plt.rcParams['axes.unicode_minus'] = False
+# pd.options.display.max_rows = 60
+# pd.set_option('display.max_columns', 500)
+# pd.set_option('display.width', 1000)
+%matplotlib inline
+
+########################## API ################################
+
+def get_code_list_has_price(code_list, price_df, start_date):
+    return price_df[start_date:][code_list].iloc[0].dropna().index
+
+def get_low_per_firm_list(start_date, price_df, num):
+    strategy_date = get_strategy_date(start_date)
+    low_per(strategy_date).index
+    return get_code_list_has_price(low_per(strategy_date).index, price_df, start_date)[:num]
+
+def get_buy_firm_count(total_pf_df):
+    buy_firm_list = list(total_pf_df.columns[total_pf_df.iloc[-1] > 0])
+    buy_firm_list.remove('cash')
+    return len(buy_firm_list)
+
+def remove_duplicate(list, check_list):
+    new_list = []
+    for item in list:
+        if item not in check_list:
+            new_list.append(item)
+            
+    return new_list
+
+def get_last_buy_date(total_pf_df, firm_code):
+    firm_buy_list = total_pf_df[firm_code][::-1]
+    first_buy_date = 0
+    for date in firm_buy_list.index:
+        buy_count = firm_buy_list.loc[date]
+        if buy_count > 0:
+            first_buy_date = date
+        else:
+            if first_buy_date != 0:
+                break
+    return first_buy_date
+
+def get_hold_firm_list(total_pf_df):
+    return list(total_pf_df.columns[total_pf_df.iloc[-1] > 0])
+
+def get_pf_profit(date, total_pf_df, price_df):
+    total_profit = 0
+    for firm_code in total_pf_df.columns:
+        if firm_code == 'cash':
+            firm_profit = total_pf_df[firm_code].loc[date]
+        else:
+            price = price_df[firm_code].fillna(0).loc[date]
+            count = total_pf_df[firm_code].loc[date]
+            firm_profit = price * count
+        total_profit = total_profit + firm_profit
+    return total_profit
+
+def get_backtest_result_from_pf(total_pf_df, price_df):
+    for num, date in enumerate(total_pf_df.index):
+        pf_profit = get_pf_profit(date, total_pf_df, price_df)
+        temp_df = pd.DataFrame({'total_portfolio':pf_profit}, index=[date])
+        if num == 0:
+            backtest_df = temp_df
+            initial_money = pf_profit
+        else:
+            backtest_df = pd.concat([backtest_df, temp_df])
+    backtest_df['day_change_rate'] = backtest_df['total_portfolio'].pct_change()
+    backtest_df['total_change_rate'] = backtest_df['total_portfolio']/initial_money - 1
+    return backtest_df
+
+def make_initial_portfolio(firm_list, start_date, price_df, initial_money):
+    strategy_price = price_df[firm_list][start_date:]
+    pf_stock_num = {}
+    stock_amount = 0
+
+    each_money = initial_money / len(firm_list)
+    for code in strategy_price.columns:
+        stock_count = int( each_money / strategy_price[code][0] )
+        pf_stock_num[code] = stock_count
+        stock_amount = stock_amount + stock_count * strategy_price[code][0]
+    cash_amount = initial_money - stock_amount
+    print('>>>>>> initial buy list (' + str(len(pf_stock_num)) + ') : ' + str(start_date))
+    print(pf_stock_num)
+    print('\n')
+
+    pf_df = pd.DataFrame(pf_stock_num, index=[strategy_price.iloc[0].name])
+    pf_df['cash'] = cash_amount
+    return pf_df
+
+def make_update_portfolio(firm_list, total_pf_df, date, price_df):
+    strategy_price = price_df[firm_list][date:]
+    pf_stock_num = {}
+    stock_amount = 0
+    total_cash = total_pf_df['cash'].iloc[-1]
+    each_money = total_cash / len(firm_list)
+#     print(each_money)
+    for code in strategy_price.columns:
+        stock_count = int( each_money / strategy_price[code][0] )
+        pf_stock_num[code] = stock_count
+        
+        if code in total_pf_df.columns:
+            total_pf_df.at[date, code] = stock_count
+        else:
+            total_pf_df[code] = 0
+            total_pf_df.at[date, code] = stock_count
+        
+        stock_amount = stock_amount + stock_count * strategy_price[code][0]
+    total_cash = total_cash - stock_amount
+    total_pf_df.at[date, 'cash'] = total_cash
+    print('>>>>>> buy list (' + str(len(pf_stock_num)) + ') : ' + str(date))
+    print(pf_stock_num)
+    print('\n')
+    return total_pf_df
+
+
+def update_portfolio(total_pf_df, today, price_df, firm_count=20, min_profit=0.5, min_days=365*2):
+    temp_df = pd.DataFrame(total_pf_df.iloc[-1]).T
+    temp_df.index = [today]
+    total_pf_df = pd.concat([total_pf_df, temp_df])
+    
+    total_cash = total_pf_df['cash'].iloc[-1]
+#     prev_date = total_pf_df.index[-1]
+    
+    sell_count = 0
+    sell_code_list = []
+    for code in total_pf_df.columns:
+        if code == 'cash': # not firm
+            continue
+            
+        buy_count = total_pf_df[code].iloc[-1] # already sell
+        if buy_count == 0:
+            continue
+            
+        last_buy_date = get_last_buy_date(total_pf_df, code)
+        if last_buy_date != 0:
+            buy_price = price_df[code].loc[last_buy_date]
+            today_price = price_df[code].loc[today]
+            profit = today_price / buy_price - 1
+            pass_enough_years_after_buy = pd.to_datetime(today) - last_buy_date > datetime.timedelta(days=min_days)
+            enough_profit = profit >= min_profit
+            if enough_profit:
+                print('>>>>>> enough profit (' + code + ') - ' + str(profit) + ' : ' + str(today))
+            if pass_enough_years_after_buy:
+                print('>>>>>> enough periods (' + code + ') - ' + str(pd.to_datetime(today) - last_buy_date) + str(today))
+            if pass_enough_years_after_buy or enough_profit:
+                sell_profit = buy_count * today_price
+                total_pf_df.at[today, code] = 0
+                total_cash = total_cash + sell_profit
+                sell_count = sell_count + 1
+                sell_code_list.append(code)
+                
+    total_pf_df.at[today, 'cash'] = total_cash  
+#     print(sell_count)
+    if sell_count > 0:
+        print('>>>>>> sell list (' + str(len(sell_code_list)) + ') : ' +  str(today))
+        print(sell_code_list)
+        print('\n')
+        firm_list = get_low_per_firm_list(today.strftime('%Y-%m-%d'), price_df, None)
+#         firm_list = get_low_per_firm_list(start_date, price_df, None)
+#         print('>>>>>> low per')
+#         print(firm_list[:20])
+        firm_list = remove_duplicate(firm_list, sell_code_list)
+        firm_list = remove_duplicate(firm_list, get_hold_firm_list(total_pf_df))
+        
+        buy_firm_count = get_buy_firm_count(total_pf_df)
+        buy_count = firm_count - buy_firm_count
+#         printㅠ('>>>> buy count : ' + str(buy_count))
+        total_pf_df = make_update_portfolio(firm_list[:buy_count], total_pf_df, today, price_df)
+    return total_pf_df
+
+
+def get_low_per_backtest_df(start_date, price_df, firm_count=20, initial_money=100000000, min_profit=0.5, min_days=365*2):
+    firm_list = get_low_per_firm_list(start_date, price_df, firm_count)
+    total_date_index = price_df[start_date:].index
+
+    for num, date in enumerate(total_date_index):
+        if num == 0:
+            total_pf_df = make_initial_portfolio(firm_list, date, price_df, initial_money)
+        else:
+            total_pf_df = update_portfolio(total_pf_df, date, price_df, firm_count, min_profit, min_days)
+            total_pf_df = total_pf_df.fillna(0)
+            aList = list(total_pf_df.columns)
+            aList.remove('cash')
+            aList.append('cash')
+            total_pf_df = total_pf_df[aList]
+    #         if date == pd.to_datetime('2017-1-5'):
+    #             break
+    return total_pf_df
+
+
+
+
+
+
+
+
+
+
+
+
+
